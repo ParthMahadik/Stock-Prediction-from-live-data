2ad8fd3cc19c295bb9602f09de4ff9d4a6b9af66
ParthMahadik <parthumahadik@gmail.com>
2020-06-28 15:15:41 +0530

    Final commit

--- final.py
+++ final.py
@@ -60,8 +60,6 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 
-rmse=np.sqrt(np.mean(predictions-y_test)**2)
-
 train = data[:train_data_len]
 valid = data[train_data_len:]
 valid['Predictions']=predictions
